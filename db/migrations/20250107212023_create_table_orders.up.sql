8cae47e05c25f6618a33db80f8285af5de40cd52
arefev.ad <anton.arefiev@alp-erp.ru>
2025-01-08 00:39:53 +0300

    order-create: created migrations

---
+++ db/migrations/20250107212023_create_table_orders.up.sql
@@ -0,0 +1,14 @@
+BEGIN;
+CREATE TABLE IF NOT EXISTS public.orders (
+    id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
+    "user_id" bigint NOT NULL,
+    "number" varchar(255) NOT NULL,
+    "status" smallint NOT NULL,
+    "uploaded_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT orders_pk PRIMARY KEY (id),
+    CONSTRAINT orders_unique UNIQUE (number),
+    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id)
+);
+COMMIT;
