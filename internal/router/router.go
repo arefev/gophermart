9569d9f73ecb84fe8dba227ba14e9217cd98725c
arefev.ad <anton.arefiev@alp-erp.ru>
2025-01-12 17:42:06 +0300

    shutdown: compress middleware

--- internal/router/router.go
+++ internal/router/router.go
@@ -4,12 +4,18 @@ import (
 	"github.com/arefev/gophermart/internal/config"
 	"github.com/arefev/gophermart/internal/middleware"
 	"github.com/go-chi/chi/v5"
+	chi_middleware "github.com/go-chi/chi/v5/middleware"
 	"go.uber.org/zap"
 )
 
 func New(log *zap.Logger, conf *config.Config) *chi.Mux {
+	const compressLevel = 5
+
 	mw := middleware.NewMiddleware(log, conf)
 	r := chi.NewRouter()
+	r.Use(chi_middleware.Logger)
+	r.Use(chi_middleware.Compress(compressLevel, "application/json", "text/html"))
+
 	log.Info("Server started")
 
 	r.Mount("/api", api(&mw))
