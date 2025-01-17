9c4f48d9d8beaef7a5066189052363fd986046ff
arefev.ad <anton.arefiev@alp-erp.ru>
2025-01-17 14:51:00 +0300

    test: refactoring

--- internal/test/register_test.go
+++ internal/test/register_test.go
@@ -22,133 +22,98 @@ import (
 )
 
 func TestUserRegisterSuccess(t *testing.T) {
-	t.Run("register success", func(t *testing.T) {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
+	type want struct {
+		userExists bool
+		status     int
+		hValue     string
+	}
 
-		conf := config.Config{
-			TokenSecret: gofakeit.DigitN(10),
-			LogLevel:    "debug",
-		}
-
-		zLog, err := logger.Build(conf.LogLevel)
-		require.NoError(t, err)
-
-		pwd := gofakeit.Password(true, true, true, true, false, 10)
-		pwdHash, err := password.Encrypt(pwd)
-		require.NoError(t, err)
-
-		user := model.User{
-			Login:    gofakeit.Username(),
-			Password: pwdHash,
-		}
-
-		tr := mock_trm.NewMockTransaction(ctrl)
-		trManager := trm.NewTrm(tr, zLog)
-		tr.EXPECT().Begin(gomock.Any()).AnyTimes()
-		tr.EXPECT().Commit(gomock.Any()).AnyTimes()
-		tr.EXPECT().Rollback(gomock.Any()).AnyTimes()
-
-		userRepo := mock_application.NewMockUserRepo(ctrl)
-		userRepo.EXPECT().Exists(gomock.Any(), user.Login).Return(false).MaxTimes(1)
-		userRepo.EXPECT().FindByLogin(gomock.Any(), user.Login).Return(&user, true).MaxTimes(1)
-		userRepo.EXPECT().Create(gomock.Any(), user.Login, gomock.Any()).Return(nil).MaxTimes(1)
-
-		app := application.App{
-			Rep: application.Repository{
-				User: userRepo,
+	tests := []struct {
+		name string
+		want want
+	}{
+		{
+			name: "register success",
+			want: want{
+				userExists: false,
+				status:     http.StatusOK,
+				hValue:     "Bearer ",
 			},
-			TrManager: trManager,
-			Log:       zLog,
-			Conf:      &conf,
-		}
-
-		r := router.New(&app)
-		srv := httptest.NewServer(r)
-		defer srv.Close()
-
-		body := `{
-			"login": "` + user.Login + `",
-			"password": "` + pwd + `"
-		}`
-
-		resp, err := resty.New().
-			R().
-			SetHeader("Content-type", "application/json").
-			SetBody(body).
-			Post(srv.URL + "/api/user/register")
-
-		require.NoError(t, err)
-		require.Equal(t, http.StatusOK, resp.StatusCode())
-
-		hAuth := resp.Header().Get("Authorization")
-		require.Contains(t, hAuth, "Bearer ")
-	})
-}
-
-func TestUserRegisterStatusConflict(t *testing.T) {
-	t.Run("register status conflict", func(t *testing.T) {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-
-		conf := config.Config{
-			TokenSecret: gofakeit.DigitN(10),
-			LogLevel:    "debug",
-		}
-
-		zLog, err := logger.Build(conf.LogLevel)
-		require.NoError(t, err)
-
-		pwd := gofakeit.Password(true, true, true, true, false, 10)
-		pwdHash, err := password.Encrypt(pwd)
-		require.NoError(t, err)
-
-		user := model.User{
-			Login:    gofakeit.Username(),
-			Password: pwdHash,
-		}
-
-		tr := mock_trm.NewMockTransaction(ctrl)
-		trManager := trm.NewTrm(tr, zLog)
-		tr.EXPECT().Begin(gomock.Any()).AnyTimes()
-		tr.EXPECT().Commit(gomock.Any()).AnyTimes()
-		tr.EXPECT().Rollback(gomock.Any()).AnyTimes()
-
-		userRepo := mock_application.NewMockUserRepo(ctrl)
-		userRepo.EXPECT().Exists(gomock.Any(), user.Login).Return(true).MaxTimes(1)
-		userRepo.EXPECT().FindByLogin(gomock.Any(), user.Login).Return(&user, true).MaxTimes(1)
-		userRepo.EXPECT().Create(gomock.Any(), user.Login, gomock.Any()).Return(nil).MaxTimes(1)
-
-		app := application.App{
-			Rep: application.Repository{
-				User: userRepo,
+		},
+		{
+			name: "register status conflict",
+			want: want{
+				userExists: true,
+				status:     http.StatusConflict,
+				hValue:     "",
 			},
-			TrManager: trManager,
-			Log:       zLog,
-			Conf:      &conf,
-		}
+		},
+	}
 
-		r := router.New(&app)
-		srv := httptest.NewServer(r)
-		defer srv.Close()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
 
-		body := `{
-			"login": "` + user.Login + `",
-			"password": "` + pwd + `"
-		}`
+			conf := config.Config{
+				TokenSecret: gofakeit.DigitN(10),
+				LogLevel:    "debug",
+			}
 
-		resp, err := resty.New().
-			R().
-			SetHeader("Content-type", "application/json").
-			SetBody(body).
-			Post(srv.URL + "/api/user/register")
+			zLog, err := logger.Build(conf.LogLevel)
+			require.NoError(t, err)
 
-		require.NoError(t, err)
-		require.Equal(t, http.StatusConflict, resp.StatusCode())
+			pwd := gofakeit.Password(true, true, true, true, false, 10)
+			pwdHash, err := password.Encrypt(pwd)
+			require.NoError(t, err)
 
-		hAuth := resp.Header().Get("Authorization")
-		require.NotContains(t, hAuth, "Bearer ")
-	})
+			user := model.User{
+				Login:    gofakeit.Username(),
+				Password: pwdHash,
+			}
+
+			tr := mock_trm.NewMockTransaction(ctrl)
+			trManager := trm.NewTrm(tr, zLog)
+			tr.EXPECT().Begin(gomock.Any()).AnyTimes()
+			tr.EXPECT().Commit(gomock.Any()).AnyTimes()
+			tr.EXPECT().Rollback(gomock.Any()).AnyTimes()
+
+			userRepo := mock_application.NewMockUserRepo(ctrl)
+			userRepo.EXPECT().Exists(gomock.Any(), user.Login).Return(tt.want.userExists).MaxTimes(1)
+			userRepo.EXPECT().FindByLogin(gomock.Any(), user.Login).Return(&user, true).MaxTimes(1)
+			userRepo.EXPECT().Create(gomock.Any(), user.Login, gomock.Any()).Return(nil).MaxTimes(1)
+
+			app := application.App{
+				Rep: application.Repository{
+					User: userRepo,
+				},
+				TrManager: trManager,
+				Log:       zLog,
+				Conf:      &conf,
+			}
+
+			r := router.New(&app)
+			srv := httptest.NewServer(r)
+			defer srv.Close()
+
+			body := `{
+				"login": "` + user.Login + `",
+				"password": "` + pwd + `"
+			}`
+
+			resp, err := resty.New().
+				R().
+				SetHeader("Content-type", "application/json").
+				SetBody(body).
+				Post(srv.URL + "/api/user/register")
+
+			require.NoError(t, err)
+			require.Equal(t, tt.want.status, resp.StatusCode())
+
+			hAuth := resp.Header().Get("Authorization")
+			require.Contains(t, hAuth, tt.want.hValue)
+		})
+	}
 }
 
 func TestUserRegisterStatusBadRequest(t *testing.T) {
